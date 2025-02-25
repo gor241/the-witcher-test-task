edd16a330d021eb5855aa9697a4c241260b5ab93
Ruslan Nuriev <93126731+gor241@users.noreply.github.com>
2025-02-25 13:07:20 +0300

    chore: Инициализация Next.js проекта

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Witcher Project",
+  description: "Test task",
+};
+
+const futuraPt = localFont({
+  src: [
+    {
+      path: "../../public/fonts/futura_pt_book.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/futura_pt_demi.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-futura-pt",
+  display: "swap", 
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ru">
+      <body className={futuraPt.variable}>
+        {children}
+      </body>
+    </html>
+  );
+}
